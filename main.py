95e3651fe5d68fcdddff4e0ee6f028db380e514c
Vandush <vandush@ymail.com>
2025-05-18 21:48:25 -0400

    CH2:L1 added constants

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #Python game library
 import pygame
 
+#Constant Variables
+from constants import *
+
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
