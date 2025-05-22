175f81d78d9e217e8777c3f2a3695b289909e1af
Vandush <vandush@ymail.com>
2025-05-21 20:26:25 -0400

    CH2:L3 set max fps

--- main.py
+++ main.py
@@ -6,15 +6,19 @@ from constants import *
 
 def main():
     pygame.init()
+    pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock =  pygame.time.Clock()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        clock.tick(60)
         pygame.display.flip()
 
 if __name__ == "__main__":
