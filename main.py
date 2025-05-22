8f260c86acffa73316cd09bb3eb0cb77174591e5
Vandush <vandush@ymail.com>
2025-05-21 22:48:28 -0400

    CH2:L6 added player and rotation

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pygame
 #Constant Variables
 from constants import *
 
+from player import *
+
 def main():
     pygame.init()
     pygame.time.Clock()
@@ -13,12 +15,17 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock =  pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
         clock.tick(60)
+        player.update(2)
+        player.draw(screen)
         pygame.display.flip()
 
 if __name__ == "__main__":
